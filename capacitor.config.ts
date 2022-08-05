56cdf50f4bb64ba47b86781290955616e0b3ceff
Andres-PeMu <andres.pena.m15@gmail.com>
2022-08-04 20:46:02 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Radio',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
